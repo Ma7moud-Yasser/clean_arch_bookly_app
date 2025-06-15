08b41a7b9498f969843b6b0c49b352f26ba4ec8e
Your Name <your_email@example.com>
2025-06-15 16:38:58 +0300

    Add BOOKLY SVG asset and update asset management for splash screen

--- lib/features/presentation/views/widgets/splash_view_body.dart
+++ lib/features/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,20 @@
+import 'package:clean_arch_bookly_app/core/styles/assets_manager.dart';
+import 'package:clean_arch_bookly_app/core/styles/size_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SvgPicture.asset(
+          SvgAssets.logo,
+          width: SizeManager.getSize(context).width * 0.5,
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:clean_arch_bookly_app/core/styles/color_manager.dart';
 import 'package:clean_arch_bookly_app/features/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
@@ -11,6 +12,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(home: const SplashView());
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(scaffoldBackgroundColor: AppColor.primary),
+      home: const SplashView(),
+    );
   }
 }
