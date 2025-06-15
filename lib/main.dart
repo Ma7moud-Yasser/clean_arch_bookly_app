188226431a4533908aba356eb99834f1902cae44
Your Name <your_email@example.com>
2025-06-15 14:36:39 +0300

    init bookly app files

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:clean_arch_bookly_app/features/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(home: const SplashView());
+  }
+}
