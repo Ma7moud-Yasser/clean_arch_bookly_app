41d4072704091de9c7578f5c7d2ed90aa1aef040
Your Name <your_email@example.com>
2025-06-15 16:46:09 +0300

    Update BOOKLY SVG asset and set theme mode in MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
       theme: ThemeData(scaffoldBackgroundColor: AppColor.primary),
       home: const SplashView(),
     );
