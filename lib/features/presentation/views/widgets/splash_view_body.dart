47ab662561177046032246ce2647668585b8e4e6
Your Name <your_email@example.com>
2025-06-15 17:20:23 +0300

    add Fade In Animation To Splash View Body

--- lib/features/presentation/views/widgets/splash_view_body.dart
+++ lib/features/presentation/views/widgets/splash_view_body.dart
@@ -3,18 +3,54 @@ import 'package:clean_arch_bookly_app/core/styles/size_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> fadeIn;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+    fadeIn = Tween<double>(begin: 0.0, end: 1.0).animate(animationController);
+    animationController.forward();
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SvgPicture.asset(
-          SvgAssets.logo,
-          width: SizeManager.getSize(context).width * 0.5,
+        child: AnimatedBuilder(
+          animation: fadeIn,
+          builder: (context, _) {
+            return FadeTransition(
+              opacity: fadeIn,
+
+              child: SvgPicture.asset(
+                SvgAssets.logo,
+                width: SizeManager.getSize(context).width * 0.5,
+              ),
+            );
+          },
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
   }
 }
