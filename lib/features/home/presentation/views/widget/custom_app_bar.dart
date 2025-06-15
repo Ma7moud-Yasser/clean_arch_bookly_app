7c33cee0895fd108af08c1b5739cd3a02559d049
Your Name <your_email@example.com>
2025-06-15 17:59:40 +0300

    refactor code

---
+++ lib/features/home/presentation/views/widget/custom_app_bar.dart
@@ -0,0 +1,30 @@
+import 'package:clean_arch_bookly_app/core/styles/assets_manager.dart';
+import 'package:clean_arch_bookly_app/core/styles/size_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        SvgPicture.asset(
+          SvgAssets.logo,
+          height: SizeManager.getSize(context).height * 0.025,
+        ),
+        GestureDetector(
+          onTap: () {
+            // Handle notification tap
+          },
+          child: SvgPicture.asset(
+            SvgAssets.search,
+            height: SizeManager.getSize(context).height * 0.025,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/home/presentation/views/widget/home_view_body.dart
+++ lib/features/home/presentation/views/widget/home_view_body.dart
@@ -1,8 +1,6 @@
-import 'package:clean_arch_bookly_app/core/styles/assets_manager.dart';
 import 'package:clean_arch_bookly_app/core/styles/padding_manager.dart';
-import 'package:clean_arch_bookly_app/core/styles/size_manager.dart';
+import 'package:clean_arch_bookly_app/features/home/presentation/views/widget/custom_app_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -13,28 +11,7 @@ class HomeViewBody extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: PaddingManager.symmetric(context: context),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SvgPicture.asset(
-                    SvgAssets.logo,
-                    height: SizeManager.getSize(context).height * 0.025,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      // Handle notification tap
-                    },
-                    child: SvgPicture.asset(
-                      SvgAssets.search,
-                      height: SizeManager.getSize(context).height * 0.025,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+          child: Column(children: [CustomAppBar()]),
         ),
       ),
     );
