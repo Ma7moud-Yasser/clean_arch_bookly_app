a9791cae990aea3bb0166d1e436135430a0c6130
Ma7moud-Yasser <your_email@example.com>
2025-06-19 23:37:39 +0300

    Add search-related strings to AppString for improved search functionality

--- lib/core/resources/app_stings.dart
+++ lib/core/resources/app_stings.dart
@@ -2,4 +2,6 @@ class AppString {
   static String appName = 'Bookly';
   static String bestSeller = 'Best Seller';
   static String youCanAlsoLike = 'You Can Also Like';
+  static String searchBooks = 'Search Books';
+  static String searchInput = 'Enter book title or author';
 }
