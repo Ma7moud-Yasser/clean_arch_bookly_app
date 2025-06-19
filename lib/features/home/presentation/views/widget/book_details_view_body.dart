345df044108c1258d3e17b9d1a8c1e4dcbefdd4d
Ma7moud-Yasser <your_email@example.com>
2025-06-19 22:47:38 +0300

    Refactor BookDetailsViewBody to use CustomScrollView for improved layout and structure

--- lib/features/home/presentation/views/widget/book_details_view_body.dart
+++ lib/features/home/presentation/views/widget/book_details_view_body.dart
@@ -1,10 +1,10 @@
 import 'package:clean_arch_bookly_app/core/styles/padding_manager.dart';
 import 'package:clean_arch_bookly_app/core/styles/sized_box_manager.dart';
-import 'package:clean_arch_bookly_app/core/styles/styles_manager.dart';
 import 'package:clean_arch_bookly_app/features/home/presentation/views/widget/book_details_action_button.dart';
 import 'package:clean_arch_bookly_app/features/home/presentation/views/widget/book_informations.dart';
 import 'package:clean_arch_bookly_app/features/home/presentation/views/widget/bool_cover.dart';
 import 'package:clean_arch_bookly_app/features/home/presentation/views/widget/custom_book_details_app_bar.dart';
+import 'package:clean_arch_bookly_app/features/home/presentation/views/widget/suggestion_book_section.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -12,65 +12,48 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: PaddingManager.symmetric(context: context),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  CustomBookDetailsAppBar(),
-                  SizedBoxManager.height(context, 20),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.3,
-
-                    child: BookCover(),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: PaddingManager.symmetric(context: context),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CustomBookDetailsAppBar(),
+                      SizedBoxManager.height(context, 30),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.3,
+                        child: BookCover(),
+                      ),
+                      SizedBoxManager.height(context, 44),
+                      BookInformations(
+                        title: "The Jungle Book",
+                        author: "Rudyard Kipling",
+                        ratting: "4.8",
+                        views: "2390",
+                      ),
+                      SizedBoxManager.height(context, 40),
+                      BookDetailsActionButton(
+                        onPressedBuy: () {},
+                        onPressedPreview: () {},
+                        price: "19.99 \$",
+                      ),
+                    ],
                   ),
-                  SizedBoxManager.height(context, 44),
-                  BookInformations(
-                    title: "The Jungle Book",
-                    author: "Rudyard Kipling",
-                    ratting: "4.8",
-                    views: "2390",
-                  ),
-                  SizedBoxManager.height(context, 40),
-                  BookDetailsActionButton(
-                    onPressedBuy: () {},
-                    onPressedPreview: () {},
-                    price: "19.99 \$",
-                  ),
-                  SizedBoxManager.height(context, 20),
-                ],
-              ),
+                ),
+                Expanded(child: SizedBoxManager.height(context, 50)),
+                SuggestionBookSection(),
+                SizedBoxManager.height(context, 40),
+              ],
             ),
-            Padding(
-              padding: PaddingManager.symmetric(context: context, vertical: 0),
-              child: Text(
-                "You Can Also Like",
-                style: StyleManager.textStyle18(
-                  context,
-                  fontFamily: AppFontFamily.montserrat,
-                  fontWeight: FontWeight.bold,
-                ).copyWith(color: Colors.white),
-              ),
-            ),
-            SizedBoxManager.height(context, 18),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.15,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) => SizedBox(child: BookCover()),
-                separatorBuilder:
-                    (context, index) => SizedBoxManager.width(context, 20),
-                itemCount: 10,
-              ),
-            ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
