faca9d990c2b654a7074c1442acf3737bcc3e6e3
Ma7moud-Yasser <your_email@example.com>
2025-06-19 23:14:33 +0300

    Refactor CustomAppBar to include padding and improve layout structure

--- lib/features/home/presentation/views/widget/custom_app_bar.dart
+++ lib/features/home/presentation/views/widget/custom_app_bar.dart
@@ -1,4 +1,6 @@
+import 'package:clean_arch_bookly_app/core/resources/route_manager.dart';
 import 'package:clean_arch_bookly_app/core/styles/assets_manager.dart';
+import 'package:clean_arch_bookly_app/core/styles/padding_manager.dart';
 import 'package:clean_arch_bookly_app/core/styles/size_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -8,23 +10,26 @@ class CustomAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        SvgPicture.asset(
-          SvgAssets.logo,
-          height: SizeManager.getSize(context).height * 0.025,
-        ),
-        GestureDetector(
-          onTap: () {
-            // Handle notification tap
-          },
-          child: SvgPicture.asset(
-            SvgAssets.search,
+    return Padding(
+      padding: PaddingManager.symmetric(context: context),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SvgPicture.asset(
+            SvgAssets.logo,
             height: SizeManager.getSize(context).height * 0.025,
           ),
-        ),
-      ],
+          GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, AppRoute.searchView.name);
+            },
+            child: SvgPicture.asset(
+              SvgAssets.search,
+              height: SizeManager.getSize(context).height * 0.025,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
