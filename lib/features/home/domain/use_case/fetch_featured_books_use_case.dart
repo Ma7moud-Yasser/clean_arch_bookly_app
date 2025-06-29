6e3ecffcf1d97749b79bff78dedc2444fc51d744
Ma7moud-Yasser <your_email@example.com>
2025-06-29 20:13:31 +0300

    Refactor FetchFeaturedBooksUseCase to extend UseCase and implement call method for consistency

--- lib/features/home/domain/use_case/fetch_featured_books_use_case.dart
+++ lib/features/home/domain/use_case/fetch_featured_books_use_case.dart
@@ -1,23 +1,16 @@
 import 'package:clean_arch_bookly_app/core/error/failure.dart';
+import 'package:clean_arch_bookly_app/core/use_case/use_case.dart';
 import 'package:clean_arch_bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:clean_arch_bookly_app/features/home/domain/repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, void> {
   final HomeRepo _repository;
 
   FetchFeaturedBooksUseCase(this._repository);
 
-  Future<Either<Failure, List<BookEntity>>> call() {
-    // check permissions
-    // if permissions are not granted, return a failure
-    // if permissions are granted, proceed to fetch featured books
-    // For simplicity, we assume permissions are always granted in this example.
-    // In a real application, you would implement permission checks here.
-    // Example:
-    // if (!permissionsGranted) {
-    //   return Left(PermissionFailure());
-    // }
-    return _repository.fetchFeaturedBooks();
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([void params]) async {
+    return await _repository.fetchFeaturedBooks();
   }
 }
