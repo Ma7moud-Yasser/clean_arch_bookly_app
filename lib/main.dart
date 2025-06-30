f059209a3afa23c05f83e63b8065f5231391be36
Ma7moud-Yasser <your_email@example.com>
2025-06-30 19:57:35 +0300

    Initialize Hive in the main function

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
   await Hive.openBox<BookEntity>(CacheKeys.featuredBook);
 
